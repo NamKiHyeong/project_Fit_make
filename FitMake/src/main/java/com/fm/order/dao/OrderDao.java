4005b9d4c114aa29e241515a8dc4431d7af94b1c
Jake-tt <109198744+Jake-tt@users.noreply.github.com>
2022-11-09 13:57:33 +0900

    Update kth part(order part) and logic test

---
+++ FitMake/src/main/java/com/fm/order/dao/OrderDao.java
@@ -0,0 +1,13 @@
+package com.fm.order.dao;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+
+import com.fm.member.model.MemberDto;
+
+public interface OrderDao {
+	
+	
+}
